e56d8b27addf13504ac112ecdb37acb5ac16065b
siduck <siduck@tutanota.com>
2023-08-12 08:59:28 +0530

    update replace_word module path

--- lua/base46/init.lua
+++ lua/base46/init.lua
@@ -173,11 +173,11 @@ M.toggle_theme = function()
   if g.nvchad_theme == theme1 then
     g.toggle_theme_icon = "   "
     vim.g.nvchad_theme = theme2
-    require("nvchad").replace_word('theme = "' .. theme1, 'theme = "' .. theme2)
+    require("nvchad.utils").replace_word('theme = "' .. theme1, 'theme = "' .. theme2)
   else
     vim.g.nvchad_theme = theme1
     g.toggle_theme_icon = "   "
-    require("nvchad").replace_word('theme = "' .. theme2, 'theme = "' .. theme1)
+    require("nvchad.utils").replace_word('theme = "' .. theme2, 'theme = "' .. theme1)
   end
 
   M.load_all_highlights()
@@ -191,7 +191,7 @@ M.toggle_transparency = function()
   local old_data = "transparency = " .. tostring(config.ui.transparency)
   local new_data = "transparency = " .. tostring(g.transparency)
 
-  require("nvchad").replace_word(old_data, new_data)
+  require("nvchad.utils").replace_word(old_data, new_data)
 end
 
 return M
